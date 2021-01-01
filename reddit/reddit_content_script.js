fc6b1895a6f9478e96bf5c149763bab65751f223
Jordan Rejaud <jrejaud@gmail.com>
2021-01-01 13:41:36 +0900

    Now working with reddit comment pages

--- reddit/reddit_content_script.js
+++ reddit/reddit_content_script.js
@@ -1,14 +1,5 @@
-const isOnSubredditPage = (url) => {
-  const splits = url.split("/")
-  if (splits.length !== 4) {
-    return false
-  }
-  if (splits[1] !== "r") {
-
-  }
-}
-
 const SUBREDDIT_PAGE_REGEX = /\/r\/\w*[\/|\w]$/m
+const COMMENTS_PAGE_REGEX = /\/r\/\w*\/comments\/\w*\/\w*[\/|\w*]$/m
 const REDDIT_RSS_BUTTON_ID = "data-make-rss-great-again"
 
 const delay = async (ms) => {
@@ -22,7 +13,12 @@ Element.prototype.remove = function () {
 }
 
 const getJoinButton = (document) => {
-  return document.querySelector("#SHORTCUT_FOCUSABLE_DIV > div:nth-child(4) > div > div > div > div._3ozFtOe6WpJEMUtxDOIvtU > div.q4a8asWOWdfdniAbgNhMh > div > div.QscnL9OySMkHhGudEvEya > div > div._1Q_zPN5YtTLQVG72WhRuf3")
+  return document.querySelector("#SHORTCUT_FOCUSABLE_DIV > div:nth-child(6) > div > div > div > div._3ozFtOe6WpJEMUtxDOIvtU > div.q4a8asWOWdfdniAbgNhMh > div > div > div > div._1Q_zPN5YtTLQVG72WhRuf3")
+}
+
+// Used to get the title text on comment pages
+const getTitleDiv = (document) => {
+  return document.querySelector("div[id*=overlay-share-menu]")
 }
 
 const createRedditRssElement = (document, imageURL) => {
@@ -31,6 +27,13 @@ const createRedditRssElement = (document, imageURL) => {
   rssImage.style.height = "30px"
   rssImage.style.marginLeft = "10px"
   rssImage.style.marginBottom = "12px"
+  return rssImage
+}
+
+const createCommentPageRssElement = (document, imageURL) => {
+  const rssImage = document.createElement("img")
+  rssImage.src = imageURL
+  rssImage.style.height = "16px"
   return rssImage
 }
 
@@ -44,7 +47,6 @@ const createRssElement = (url, image) => {
 
 const main = async () => {
 
-  console.log("Remove prior buttons")
   // Remove previous buttons
   const previousButtons = [...document.querySelectorAll("[" + REDDIT_RSS_BUTTON_ID + "]")]
   previousButtons.forEach(item => {
@@ -53,8 +55,7 @@ const main = async () => {
 
   // If you are on a subreddit's page
   const isOnSubredditPage = SUBREDDIT_PAGE_REGEX.test(document.location.pathname)
-
-  console.log("on subreddit page: ", isOnSubredditPage)
+  const isOnCommentPage = COMMENTS_PAGE_REGEX.test(document.location.pathname)
 
   if (isOnSubredditPage) {
     let joinButton = getJoinButton(document)
@@ -65,16 +66,31 @@ const main = async () => {
       joinButton = getJoinButton(document)
     }
 
-    console.log("Adding RSS button")
-
     // Add new button
     const imageURL = chrome.runtime.getURL("img/rss_icon.svg")
     const rssURL = document.URL + ".rss"
     const rssElement = createRssElement(rssURL, createRedditRssElement(document, imageURL))
     joinButton.parentElement.appendChild(rssElement)
   }
-}
 
+  else if (isOnCommentPage) {
+
+    let titleDiv = getTitleDiv(document)
+
+    // Wait for a little bit, or else there's an issue
+    delay(2000)
+
+    while (!titleDiv) {
+      await delay(500)
+      titleDiv = getTitleDiv(document)
+    }
+
+    const imageURL = chrome.runtime.getURL("img/rss_icon.svg")
+    const rssURL = document.URL + ".rss"
+    const rssElement = createRssElement(rssURL, createCommentPageRssElement(document, imageURL))
+    titleDiv.insertAdjacentElement("afterend", rssElement)
+  }
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message !== "make-reddit-great-again") {
